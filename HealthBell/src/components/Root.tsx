ea8f4f26de7dd7f47bc2b750a6e6a00b1f8b684f
Gaith Alsahaf <gaith.alsahaf@student.hogent.be>
2025-11-09 18:28:42 +0100

    Install React Query and Axios; add React Query provider at app root (not yet used, pending backend integration)

--- HealthBell/src/components/Root.tsx
+++ HealthBell/src/components/Root.tsx
@@ -2,12 +2,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import TabsNavigator from '../navigation/TabsNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Root() {
   return (
-     <SafeAreaProvider>
+    <SafeAreaProvider>
       <NavigationContainer>
-       <TabsNavigator />
+        <QueryClientProvider client={queryClient}>
+          <TabsNavigator />
+        </QueryClientProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
